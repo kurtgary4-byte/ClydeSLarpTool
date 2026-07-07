64f0a6f33e9eacf5e10752a901c86fb956453487
kurtgary4-byte <kurtgary4@gmail.com>
2026-07-07 14:27:12 +0200

    initial commit

---
+++ wordspam.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+  int main(){
+     char word[80] = "";
+     char response = '\0';
+
+
+     printf("Enter word: ");
+     fgets(word, sizeof word, stdin);
+     word[strlen(word) - 1] = '\0';
+
+     printf("Are you ready?(Y/N): ");
+     scanf("%c", &response);
+
+     if(response != 'Y' && response != 'y'){
+
+         printf("Why did you even run this program then :sob:");
+
+     }else{
+      
+             while(1 == 1){
+
+                 printf("%s", word);
+
+             }
+
+
+     }
+
+       
+
+
+    return 0;
+  }
